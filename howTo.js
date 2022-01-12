9176e7c3c52f77d7d699550f6097bb740ea70874
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-01-12 20:49:33 +0100

    added comments js, spell check

--- howTo.js
+++ howTo.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", start);
 
+// assigns active and inactive classes to style tabs and info
 function start() {
     console.log("start tabs");
     document.querySelector("#lenders-tab").classList.add("active-tab");
@@ -8,11 +9,13 @@ function start() {
     document.querySelector(".inactive-tab").addEventListener("click", toggleTab);
 }
 
+// sets me up to save time by writing a shorter expression when I need these elements
 const lendersTab = document.querySelector("#lenders-tab");
 const borrowersTab = document.querySelector("#borrowers-tab");
 const lendersInfo = document.querySelector("#lenders-info")
 const borrowersInfo = document.querySelector("#borrowers-info");
 
+// toggles the active and inactive tabs, restarts itself, calls function for info sections
 function toggleTab() {
     console.log("toggle tab");
     document.querySelector(".inactive-tab").removeEventListener("click", toggleTab);
@@ -24,6 +27,7 @@ function toggleTab() {
     toggleInfo();
 }
 
+// toggles which set of steps (info) are shown
 function toggleInfo() {
     console.log("toggle info");
     lendersInfo.classList.toggle("hidden");
